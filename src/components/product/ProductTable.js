cc50107014fa13b54a95243686c8e6d6ca539d0d
Husnain Gauher <husnaingauher@Husnains-MacBook-Pro.local>
2022-12-04 19:53:42 +0500

    Ref

---
+++ src/components/product/ProductTable.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  TableCell,
+  TableBody,
+  TableRow,
+  Badge,
+  Avatar,
+} from '@windmill/react-ui';
+import { FiZoomIn } from 'react-icons/fi';
+
+import Tooltip from '../tooltip/Tooltip';
+import MainModal from '../modal/MainModal';
+import MainDrawer from '../drawer/MainDrawer';
+import ProductDrawer from '../drawer/ProductDrawer';
+import ShowHideButton from '../table/ShowHideButton';
+import EditDeleteButton from '../table/EditDeleteButton';
+import useToggleDrawer from '../../hooks/useToggleDrawer';
+
+const ProductTable = ({ products }) => {
+  const { title, serviceId, handleModalOpen, handleUpdate } = useToggleDrawer();
+
+  return (
+    <>
+      <MainModal id={serviceId} title={title} />
+      <MainDrawer>
+        <ProductDrawer id={serviceId} />
+      </MainDrawer>
+      <TableBody>
+        {products?.map((product, i) => (
+          <TableRow key={i + 1}>
+            
+            <TableCell>
+              <div className="flex items-center">
+                <Avatar
+                  className="hidden p-1 mr-2 md:block bg-gray-50 shadow-none"
+                  src={product.image}
+                  alt={product.title}
+                />
+                <div>
+                  <h2 className="text-sm font-medium">{product.title}</h2>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell>
+              <span className="text-sm">{product.parent}</span>
+            </TableCell>
+
+          
+
+            <TableCell>
+              <span className="text-sm">{product.quantity}</span>
+            </TableCell>
+            <TableCell>
+              {product.quantity > 0 ? (
+                <Badge type="success">Selling</Badge>
+              ) : (
+                <Badge type="danger">Sold Out</Badge>
+              )}
+            </TableCell>
+
+        
+            <TableCell>
+              <Link
+                to={`/product/${product._id}`}
+                className="flex justify-start text-center text-gray-400 hover:text-green-600 ml-4"
+              >
+                <Tooltip
+                  id="details"
+                  Icon={FiZoomIn}
+                  title="Details"
+                  bgColor="#10B981"
+                />
+              </Link>
+            </TableCell>
+            <TableCell>
+              <ShowHideButton id={product._id} status={product.status} />
+            </TableCell>
+            <TableCell>
+              <EditDeleteButton
+                id={product._id}
+                title={product.title}
+                handleUpdate={handleUpdate}
+                handleModalOpen={handleModalOpen}
+              />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </>
+  );
+};
+
+export default React.memo(ProductTable);
