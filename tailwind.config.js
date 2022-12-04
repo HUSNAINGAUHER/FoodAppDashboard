1c672887c50686db84da5c9f4223bf2655f7621e
Husnain Gauher <husnaingauher@Husnains-MacBook-Pro.local>
2022-12-04 20:04:25 +0500

    Ref

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,6 @@ module.exports = windmill({
         sans: ['Open Sans', 'sans-serif'],
         serif: ['Inter', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-        green: {
-          500: '#07A32A'}},
       boxShadow: {
         bottom:
           '0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)',
