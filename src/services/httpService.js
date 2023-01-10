e1305024f54c373d9d8e36641e1119f52db004f9
Husnain Gauher <husnaingauher@Husnains-MacBook-Pro.local>
2023-01-10 20:30:18 +0500

    ref

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const instance = axios.create({
-  baseURL: "https://foodappbackend.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   timeout: 500000,
   headers: {
     Accept: "application/json",
